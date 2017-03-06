1c5ca39009a8316b91f17c30bc302e7eabb0439d
jc961 <J.Chatfield1@uni.brighton.ac.uk>
2017-03-06 12:26:37 +0000

    Add project files.

---
+++ ci253/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ci253
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("testtest");
+            Console.WriteLine("testtest");
+            Console.WriteLine("testtest");
+        }
+    }
+}
